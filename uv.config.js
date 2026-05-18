0b77540d664628e94697501174779c73a00f7778
Monts0-32 <brooksmonty87@gmail.com>
2026-05-18 22:05:32 +0100

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: '/bare/',
+    bare: 'https://ada-relay.andrewdinglearchive.workers.dev/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js.map',
